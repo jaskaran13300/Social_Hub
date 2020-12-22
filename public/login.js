0ed264164eeb0bd9bca58e9257e5d29b870185d9
Avnish-Dewan <avnish171167.cse@chitkara.edu.in>
2020-12-22 18:51:22 +0530

    Added public folder

---
+++ public/login.js
@@ -0,0 +1,34 @@
+var login=document.getElementById("loginbtn");
+var username=document.getElementById("username");
+var pass=document.getElementById("password");
+var tasks=[];
+login.addEventListener("click",function(event){
+    if(username.value==""||pass.value=="")
+        alert("Please insert all values");
+    else{
+        var ob=new Object();
+        ob.username1=username.value;
+        ob.pass1=pass.value;
+        console.log(ob);
+       var request = new XMLHttpRequest();
+    var file_name = "/productlogin";
+    request.open('POST',file_name);
+        request.setRequestHeader("Content-Type","application/json");
+    request.send(JSON.stringify(ob))
+    request.addEventListener("load",function()
+    {
+        if(request.responseText=="1")
+            window.location="/profile";
+        else if(request.responseText=="-1")
+        window.location="/deactivate";
+        else if(request.responseText=="new")
+        window.location="/usercommunities";
+        else if(request.responseText=="user")
+         window.location="/simpleusercommunities";
+        else if(request.responseText=="update")
+            window.location="/firstuser";
+         else
+                alert("Invalid username");
+    }); 
+    }
+});
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express=require('express')
+var path = require('path')
 var app=express();
 var port=5000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.listen(port,()=>{
     console.log(`App is running on port `+port);
 });
