3f4b9b9283735f5b97704befbf49b1593acd498a
Avnish-Dewan <avnish171167.cse@chitkara.edu.in>
2020-12-22 18:09:39 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express')
 var app=express();
-var port=5000;
+var port=5000 || process.env.PORT;
 app.listen(port,()=>{
     console.log(`App is running on port `+port);
-})
+});
